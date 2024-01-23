7cc110b71e42844ca49dac68ec84264ed58d24a0
rudokemper <rtakemper@gmail.com>
2024-01-23 08:44:35 -0500

    Add rendering support for GeoJSON, font glyph, sprite

--- src/render.js
+++ src/render.js
@@ -1,149 +1,21 @@
 import fs from "fs";
 import path from "path";
 import sharp from "sharp";
-import zlib from "zlib";
 import maplibre from "@maplibre/maplibre-gl-native";
 import MBTiles from "@mapbox/mbtiles";
 
-import {
-  calculateTileRangeForBounds,
-  convertTilesToCoordinates,
-} from "./tile_calculations.js";
+import { calculateTileRangeForBounds, calculateNormalizedCenterCoords } from "./tile_calculations.js";
+import { requestHandler } from "./request_resources.js";
 
-const TILE_REGEXP = RegExp("mbtiles://([^/]+)/(\\d+)/(\\d+)/(\\d+)");
 const MBTILES_REGEXP = /mbtiles:\/\/(\S+?)(?=[/"]+)/gi;
-
-const isMBTilesURL = (url) => url.startsWith("mbtiles://");
-
-// Split out mbtiles service name from the URL
-const resolveNamefromURL = (url) => url.split("://")[1].split("/")[0];
-
-// Resolve a URL of a local mbtiles file to a file path
-// Expected to follow this format "mbtiles://<service_name>/*"
-const resolveMBTilesURL = (tilePath, url) =>
-  /*
-   * @param {String} tilePath - path containing mbtiles files
-   * @param {String} url - url of a data source in style.json file.
-   */
-  path.format({
-    dir: tilePath,
-    name: resolveNamefromURL(url),
-    ext: ".mbtiles",
-  });
-
-// Given a URL to a local mbtiles file, get the TileJSON for that to load correct tiles.
-const getLocalTileJSON = (tilePath, url, callback) => {
-  /*
-   * @param {String} tilePath - path containing mbtiles files.
-   * @param {String} url - url of a data source in style.json file.
-   * @param {function} callback - function to call with (err, {data}).
-   */
-  const mbtilesFilename = resolveMBTilesURL(tilePath, url);
-  const service = resolveNamefromURL(url);
-
-  new MBTiles(mbtilesFilename, (err, mbtiles) => {
-    if (err) {
-      callback(err);
-      return null;
-    }
-
-    mbtiles.getInfo((infoErr, info) => {
-      if (infoErr) {
-        callback(infoErr);
-        return null;
-      }
-
-      const { minzoom, maxzoom, center, bounds, format } = info;
-
-      const ext = format === "pbf" ? ".pbf" : "";
-
-      const tileJSON = {
-        tilejson: "1.0.0",
-        tiles: [`mbtiles://${service}/{z}/{x}/{y}${ext}`],
-        minzoom,
-        maxzoom,
-        center,
-        bounds,
-      };
-
-      callback(null, { data: Buffer.from(JSON.stringify(tileJSON)) });
-      return null;
-    });
-
-    return null;
-  });
-};
-
-// Fetch a tile from a local mbtiles file.
-const getLocalTile = (tilePath, url, callback) => {
-  /*
-   * @param {String} tilePath - path containing mbtiles files.
-   * @param {String} url - url of a data source in style.json file.
-   * @param {function} callback - function to call with (err, {data}).
-   */
-  const matches = url.match(TILE_REGEXP);
-  const [z, x, y] = matches.slice(matches.length - 3, matches.length);
-  const isVector = path.extname(url) === ".pbf";
-  const mbtilesFile = resolveMBTilesURL(tilePath, url);
-
-  new MBTiles(mbtilesFile, (err, mbtiles) => {
-    if (err) {
-      callback(err);
-      return null;
-    }
-
-    mbtiles.getTile(z, x, y, (tileErr, data) => {
-      if (tileErr) {
-        callback(null, {});
-        return null;
-      }
-
-      if (isVector) {
-        // if the tile is compressed, unzip it (for vector tiles only!)
-        zlib.unzip(data, (unzipErr, unzippedData) => {
-          callback(unzipErr, { data: unzippedData });
-        });
-      } else {
-        callback(null, { data });
-      }
-
-      return null;
-    });
-
-    return null;
-  });
-};
-
-// requestHandler constructs a request handler for the map to load resources.
-const requestHandler =
-  (tilePath) =>
-  ({ url, kind }, callback) => {
-    try {
-      if (kind === 2 || kind === 3) {
-        // source or tile
-        if (isMBTilesURL(url)) {
-          kind === 2
-            ? getLocalTileJSON(tilePath, url, callback)
-            : getLocalTile(tilePath, url, callback);
-        } else {
-          const msg = `Error: Only local mbtiles URLs are supported. Received: ${url}`;
-          throw new Error(msg);
-        }
-      } else {
-        const msg = `Error: Request kind not handled: ${kind}`;
-        throw new Error(msg);
-      }
-    } catch (err) {
-      const msg = `Error while making resource request to: ${url}\n${err}`;
-      return callback(msg);
-    }
-  };
 
 // Render the map, returning a Promise.
 const renderMap = (map, options) => {
   return new Promise((resolve, reject) => {
     map.render(options, (err, buffer) => {
       if (err) {
+        console.error("Error during map rendering:", err);
+        map.release();
         return reject(err);
       }
       return resolve(buffer);
@@ -152,28 +24,10 @@ const renderMap = (map, options) => {
 };
 
 // Render map tile for a given style, zoom level, and tile coordinates
-const renderTile = async (style, tilePath, zoom, x, y) => {
+const renderTile = async (style, sourceDir, styleDir, zoom, x, y) => {
   const tileSize = 512;
 
-  // Calculate longitude and latitude from tile x, y, and zoom
-  const nw = convertTilesToCoordinates(x, y, zoom);
-  const se = convertTilesToCoordinates(x + 1, y + 1, zoom);
-
-  // Normalize latitude to the Mercator projection
-  // More about mercator tile normalization: https://maplibre.org/maplibre-native/docs/book/design/coordinate-system.html
-  const mercatorNwY = Math.log(
-    Math.tan(Math.PI / 4 + (nw.lat * Math.PI) / 360)
-  );
-  const mercatorSeY = Math.log(
-    Math.tan(Math.PI / 4 + (se.lat * Math.PI) / 360)
-  );
-  const avgMercatorY = (mercatorNwY + mercatorSeY) / 2;
-  const centerLat = (Math.atan(Math.exp(avgMercatorY)) * 360) / Math.PI - 90;
-
-  // Longitude remains a simple average
-  const centerLon = (nw.lon + se.lon) / 2;
-
-  const center = [centerLon, centerLat];
+  const center = calculateNormalizedCenterCoords(x, y, zoom);
 
   // Create a new MapLibre instance with specified size and other parameters
   // In MapLibre server-side rendering, methods like resize, setZoom, and setCenter are not available
@@ -181,19 +35,14 @@ const renderTile = async (style, tilePath, zoom, x, y) => {
   // and release the map instance after rendering.
   // MapLibre native documentation: https://github.com/maplibre/maplibre-native/blob/main/platform/node/README.md
   const map = new maplibre.Map({
-    request: requestHandler(tilePath),
+    request: requestHandler(sourceDir, styleDir),
     ratio: 1,
   });
 
   map.load(style);
 
   // Render the map to a buffer
-  const buffer = await renderMap(map, {
-    zoom: zoom,
-    center: center,
-    height: tileSize,
-    width: tileSize,
-  });
+  const buffer = await renderMap(map, { zoom: zoom, center: center, height: tileSize, width: tileSize });
 
   // Clean up the map instance to free resources
   map.release();
@@ -204,14 +53,7 @@ const renderTile = async (style, tilePath, zoom, x, y) => {
 };
 
 // Construct MBTiles file from a given style, bounds, and zoom range
-const generateMBTiles = async (
-  style,
-  tilePath,
-  bounds,
-  minZoom,
-  maxZoom,
-  output
-) => {
+const generateMBTiles = async (style, sourceDir, styleDir, bounds, minZoom, maxZoom, output) => {
   const outputPath = `outputs/${output}.mbtiles`;
 
   // Create a new MBTiles file
@@ -232,13 +74,7 @@ const generateMBTiles = async (
       throw err;
     } else {
       mbtiles.putInfo(
-        {
-          name: output,
-          format: "jpg",
-          minzoom: minZoom,
-          maxzoom: maxZoom,
-          type: "overlay",
-        },
+        { name: output, format: "jpg", minzoom: minZoom, maxzoom: maxZoom, type: "overlay" },
         (err) => {
           if (err) throw err;
         }
@@ -250,17 +86,14 @@ const generateMBTiles = async (
   for (let zoom = minZoom; zoom <= maxZoom; zoom++) {
     console.log(`Rendering zoom level ${zoom}...`);
     // Calculate tile range for this zoom level based on bounds
-    const { minX, minY, maxX, maxY } = calculateTileRangeForBounds(
-      bounds,
-      zoom
-    );
+    const { minX, minY, maxX, maxY } = calculateTileRangeForBounds(bounds, zoom);
 
     // Iterate over tiles within the range
     for (let x = minX; x <= maxX; x++) {
       for (let y = minY; y <= maxY; y++) {
         try {
           // Render the tile
-          const tileBuffer = await renderTile(style, tilePath, zoom, x, y);
+          const tileBuffer = await renderTile(style, sourceDir, styleDir, zoom, x, y);
 
           // Write the tile to the MBTiles file
           mbtiles.putTile(zoom, x, y, tileBuffer, (err) => {
@@ -313,18 +146,11 @@ const generateJPG = async (buffer, width, height, ratio) => {
     .toBuffer();
 };
 
-export const renderMBTiles = async (
-  style,
-  bounds,
-  minZoom,
-  maxZoom,
-  tilePath,
-  output
-) => {
+export const renderMBTiles = async (style, bounds, minZoom, maxZoom, sourceDir, styleDir, output) => {
   console.log("Starting rendering...");
 
   const localMbtilesMatches = JSON.stringify(style).match(MBTILES_REGEXP);
-  if (localMbtilesMatches && !tilePath) {
+  if (localMbtilesMatches && !sourceDir) {
     const msg = "Style has local mbtiles file sources, but no tilePath is set";
     throw new Error(msg);
   }
@@ -333,7 +159,7 @@ export const renderMBTiles = async (
     localMbtilesMatches.forEach((name) => {
       const mbtileFilename = path.normalize(
         path.format({
-          dir: tilePath,
+          dir: sourceDir,
           name: name.split("://")[1],
           ext: ".mbtiles",
         })
@@ -342,14 +168,14 @@ export const renderMBTiles = async (
         const msg = `Mbtiles file ${path.format({
           name,
           ext: ".mbtiles",
-        })} in style file is not found in: ${path.resolve(tilePath)}`;
+        })} in style file is not found in: ${path.resolve(sourceDir)}`;
         throw new Error(msg);
       }
     });
   }
 
   try {
-    await generateMBTiles(style, tilePath, bounds, minZoom, maxZoom, output);
+    await generateMBTiles(style, sourceDir, styleDir, bounds, minZoom, maxZoom, output);
   } catch (error) {
     console.error("Error generating MBTiles:", error);
   }
---
+++ src/request_resources.js
@@ -0,0 +1,235 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+import MBTiles from "@mapbox/mbtiles";
+
+// Validations for source URLs
+const TILE_REGEXP = RegExp("mbtiles://([^/]+)/(\\d+)/(\\d+)/(\\d+)");
+const isMBTilesURL = (url) => url.startsWith("mbtiles://");
+const isGeoJSONURL = (url) => url.endsWith(".geojson");
+
+// Split out mbtiles service name from the URL
+const resolveNamefromURL = (url) => url.split("://")[1].split("/")[0];
+
+// Resolve a URL of a local mbtiles file to a file path
+// Expected to follow this format "mbtiles://<service_name>/*"
+const resolveMBTilesURL = (sourceDir, url) =>
+  /*
+   * @param {String} sourceDir - path containing mbtiles files
+   * @param {String} url - url of a data source in style.json file.
+   */
+  path.format({
+    dir: sourceDir,
+    name: resolveNamefromURL(url),
+    ext: ".mbtiles",
+  });
+
+// Given a URL to a local sprite image, get the image data.
+const getLocalSpriteImage = (styleDir, url, callback) => {
+  const spriteImagePath = path.join(styleDir, `${url}`);
+
+  fs.readFile(spriteImagePath, (err, data) => {
+    if (err) {
+      callback(err);
+      return null;
+    }
+    callback(null, data);
+    return null;
+  });
+};
+
+// Given a URL to a local sprite JSON, get the JSON data.
+// TODO: currently, any styles with sprites defined will
+// fail to render. The callback in this function does
+// correctly return a buffer of the sprite JSON, but
+// Maplibre just hangs when trying to render it.
+// No errors are thrown, and the process never exits.
+const getLocalSpriteJSON = (styleDir, url, callback) => {
+  const spriteJsonPath = path.join(styleDir, `${url}`);
+  
+  fs.readFile(spriteJsonPath, (err, data) => {
+    if (err) {
+      callback(err);
+      return null;
+    }
+    callback(null, { data });
+    return null;
+  });
+};
+
+// Given a URL to a local glyph, get the glyph data.
+const getLocalGlyph = (styleDir, url, callback) => {
+  const decodedUrl = decodeURIComponent(url);
+  const glyphPath = path.join(styleDir, `${decodedUrl}`);
+
+  fs.readFile(glyphPath, (err, data) => {
+    if (err) {
+      callback(err);
+      return null;
+    }
+    callback(null, { data });
+    return null;
+  });
+};
+
+// Given a URL to a local GeoJSON file, get the GeoJSON for that to load correct tiles.
+const getLocalGeoJSON = (sourceDir, url, callback) => {
+  /*
+   * @param {String} geojsonPath - path containing GeoJSON files.
+   * @param {String} url - url of a data source in style.json file.
+   * @param {function} callback - function to call with (err, {data}).
+   */
+  const geojsonFilename = path.normalize(
+    path.format({
+      dir: sourceDir,
+      name: url
+    })
+  );
+
+  fs.readFile(geojsonFilename, (err, data) => {
+    if (err) {
+      callback(err);
+      return null;
+    }
+    callback(null, { data });
+    return null;
+  });
+};
+
+// Given a URL to a local mbtiles file, get the TileJSON for that to load correct tiles.
+const getLocalTileJSON = (sourceDir, url, callback) => {
+  /*
+   * @param {String} sourceDir - path containing mbtiles files.
+   * @param {String} url - url of a data source in style.json file.
+   * @param {function} callback - function to call with (err, {data}).
+   */
+  const mbtilesFilename = resolveMBTilesURL(sourceDir, url);
+  const service = resolveNamefromURL(url);
+
+  new MBTiles(mbtilesFilename, (err, mbtiles) => {
+    if (err) {
+      callback(err);
+      return null;
+    }
+
+    mbtiles.getInfo((infoErr, info) => {
+      if (infoErr) {
+        callback(infoErr);
+        return null;
+      }
+
+      const { minzoom, maxzoom, center, bounds, format } = info;
+
+      const ext = format === "pbf" ? ".pbf" : "";
+
+      const tileJSON = {
+        tilejson: "1.0.0",
+        tiles: [`mbtiles://${service}/{z}/{x}/{y}${ext}`],
+        minzoom,
+        maxzoom,
+        center,
+        bounds,
+      };
+
+      callback(null, { data: Buffer.from(JSON.stringify(tileJSON)) });
+      return null;
+    });
+
+    return null;
+  });
+};
+
+// Fetch a tile from a local mbtiles file.
+const getLocalTile = (sourceDir, url, callback) => {
+  /*
+   * @param {String} sourceDir - path containing mbtiles files.
+   * @param {String} url - url of a data source in style.json file.
+   * @param {function} callback - function to call with (err, {data}).
+   */
+  const matches = url.match(TILE_REGEXP);
+  const [z, x, y] = matches.slice(matches.length - 3, matches.length);
+  const isVector = path.extname(url) === ".pbf";
+  const mbtilesFile = resolveMBTilesURL(sourceDir, url);
+
+  new MBTiles(mbtilesFile, (err, mbtiles) => {
+    if (err) {
+      callback(err);
+      return null;
+    }
+
+    mbtiles.getTile(z, x, y, (tileErr, data) => {
+      if (tileErr) {
+        callback(null, {});
+        return null;
+      }
+
+      if (isVector) {
+        // if the tile is compressed, unzip it (for vector tiles only!)
+        zlib.unzip(data, (unzipErr, unzippedData) => {
+          callback(unzipErr, { data: unzippedData });
+        });
+      } else {
+        callback(null, { data });
+      }
+
+      return null;
+    });
+
+    return null;
+  });
+};
+
+// requestHandler constructs a request handler for the map to load resources.
+// More about request types (kinds): https://github.com/maplibre/maplibre-native/blob/main/platform/node/README.md
+export const requestHandler = (sourceDir, styleDir) => ({ url, kind }, callback) => {
+    try {
+      switch (kind) {
+        case 2: {
+          // source
+          if (isMBTilesURL(url)) {
+              getLocalTileJSON(sourceDir, url, callback);
+          } else if (isGeoJSONURL(url)) {
+              getLocalGeoJSON(sourceDir, url, callback);
+          } else {
+              const msg = `Only local sources are currently supported. Received: ${url}`;
+              throw new Error(msg);
+          }
+          break;
+        }
+        case 3: {
+          // tile
+          if (isMBTilesURL(url)) {
+              getLocalTile(sourceDir, url, callback);
+          } else if (isGeoJSONURL(url)) {
+              getLocalGeoJSON(sourceDir, url, callback);
+          } else {
+              const msg = `Only local tiles are currently supported. Received: ${url}`;
+              throw new Error(msg);
+          }
+          break;
+        }
+        case 4: {
+          // glyph
+          getLocalGlyph(styleDir, url, callback);
+          break;
+        }
+        case 5: {
+          // sprite image
+          getLocalSpriteImage(styleDir, url, callback);
+          break;
+        }
+        case 6: {
+          // sprite json
+          getLocalSpriteJSON(styleDir, url, callback);
+          break;
+        }
+        default: {
+            const msg = `Request kind not handled: ${kind}`;
+            throw new Error(msg);
+        }
+      }
+    } catch (err) {
+      const msg = `Error while making resource request to: ${url}\n${err}`;
+      return callback(msg);
+    }
+  };
