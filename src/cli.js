c993f8e826eea6707324fa41210f38c4af089ed4
luandro <luandro@gmail.com>
2024-01-24 13:28:10 -0300

    Add linting with prettier and pre-commit lint check with husky and lint-staged (#6)

--- src/cli.js
+++ src/cli.js
@@ -1,42 +1,69 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import path from 'path';
-import { program } from"commander";
-import packageJson from '../package.json' assert { type: 'json' };
+import fs from "fs";
+import path from "path";
+import { program } from "commander";
+import packageJson from "../package.json" assert { type: "json" };
 
-import { initiateRendering } from './render.js';
+import { initiateRendering } from "./render.js";
 
-const parseListToFloat = (text) => text.split(',').map(Number)
+const parseListToFloat = (text) => text.split(",").map(Number);
 
 program
   .version(packageJson.version)
   .name("mbgl-tile-render")
-    .description("Render styled Maplibre GL map tiles")
-  .requiredOption("-s ,--style <type>", "Are you providing your own map style? If not, one will be generated using your sources. (required, 'yes' or 'no')", function(value) {
-    const validSources = ['yes', 'no'];
-    value = value.toLowerCase();
-    if (!validSources.includes(value)) {
-      throw new Error('Invalid answer. It can only be yes or no.');
-    }
-    return value;
-  })    
+  .description("Render styled Maplibre GL map tiles")
+  .requiredOption(
+    "-s ,--style <type>",
+    "Are you providing your own map style? If not, one will be generated using your sources. (required, 'yes' or 'no')",
+    function (value) {
+      const validSources = ["yes", "no"];
+      value = value.toLowerCase();
+      if (!validSources.includes(value)) {
+        throw new Error("Invalid answer. It can only be yes or no.");
+      }
+      return value;
+    },
+  )
   .option("-l, --stylelocation <type>", "Location of your provided map style")
-  .option("-i, --stylesources <type>", "Directory where any local source files specified in your provided style are located")
-  .option("-O, --onlinesource <type>", "Online source type. Options: bing", function(value) {
-    const validSources = ['bing'];
-    value = value.toLowerCase();
-    if (!validSources.includes(value)) {
-      throw new Error('Invalid online source. It can only be bing.');
-    }
-    return value;
-  })
+  .option(
+    "-i, --stylesources <type>",
+    "Directory where any local source files specified in your provided style are located",
+  )
+  .option(
+    "-O, --onlinesource <type>",
+    "Online source type. Options: bing",
+    function (value) {
+      const validSources = ["bing"];
+      value = value.toLowerCase();
+      if (!validSources.includes(value)) {
+        throw new Error("Invalid online source. It can only be bing.");
+      }
+      return value;
+    },
+  )
   .option("-k, --apikey <type>", "API key for your online source (optional)")
-  .option("-a, --overlay <type>", "Feature layer to overlay on top of the online source (must be a GeoJSON object)")
-  .requiredOption("-b, --bounds <type>", "Bounding box in WSEN format, comma separated (required)", parseListToFloat)
-  .option("-z, --minzoom <number>", "Minimum zoom level (default 0)", parseInt, 0)
-  .requiredOption("-Z, --maxzoom <number>", "Maximum zoom level (required)", parseInt)
-  .option("-o, --output <type>", "Output name (default 'output')", "output")
+  .option(
+    "-a, --overlay <type>",
+    "Feature layer to overlay on top of the online source (must be a GeoJSON object)",
+  )
+  .requiredOption(
+    "-b, --bounds <type>",
+    "Bounding box in WSEN format, comma separated (required)",
+    parseListToFloat,
+  )
+  .option(
+    "-z, --minzoom <number>",
+    "Minimum zoom level (default 0)",
+    parseInt,
+    0,
+  )
+  .requiredOption(
+    "-Z, --maxzoom <number>",
+    "Maximum zoom level (required)",
+    parseInt,
+  )
+  .option("-o, --output <type>", "Output name (default 'output')", "output");
 
 program.parse(process.argv);
 
@@ -53,72 +80,88 @@ const minZoom = options.minzoom;
 const maxZoom = options.maxzoom;
 const output = options.output;
 
-if (styleProvided === 'yes' && (!styleLocation || !sourceDir)) {
-    raiseError('You must provide a style location and a source directory if you are providing your own style')
+if (styleProvided === "yes" && (!styleLocation || !sourceDir)) {
+  raiseError(
+    "You must provide a style location and a source directory if you are providing your own style",
+  );
 }
 
-if (styleProvided === 'no' && !onlineSource) {
-    raiseError('You must provide an online source if you are not providing your own style')
+if (styleProvided === "no" && !onlineSource) {
+  raiseError(
+    "You must provide an online source if you are not providing your own style",
+  );
 }
 
 if (minZoom !== null && (minZoom < 0 || minZoom > 22)) {
-    raiseError(`minZoom level is outside supported range (0-22): ${minZoom}`)
+  raiseError(`minZoom level is outside supported range (0-22): ${minZoom}`);
 }
 
 if (maxZoom !== null && (maxZoom < 0 || maxZoom > 22)) {
-    raiseError(`maxZoom level is outside supported range (0-22): ${maxZoom}`)
+  raiseError(`maxZoom level is outside supported range (0-22): ${maxZoom}`);
 }
 
 if (bounds !== null) {
-    if (bounds.length !== 4) {
-        raiseError(
-            `Bounds must be west,south,east,north.  Invalid value found: ${[
-                ...bounds,
-            ]}`
-        )
-    }
+  if (bounds.length !== 4) {
+    raiseError(
+      `Bounds must be west,south,east,north.  Invalid value found: ${[
+        ...bounds,
+      ]}`,
+    );
+  }
 
-    bounds.forEach((b) => {
-        if (!Number.isFinite(b)) {
-            raiseError(
-                `Bounds must be valid floating point values.  Invalid value found: ${[
-                    ...bounds,
-                ]}`
-            )
-        }
-        return null
-    })
+  bounds.forEach((b) => {
+    if (!Number.isFinite(b)) {
+      raiseError(
+        `Bounds must be valid floating point values.  Invalid value found: ${[
+          ...bounds,
+        ]}`,
+      );
+    }
+    return null;
+  });
 
-    const [west, south, east, north] = bounds
-    if (west === east) {
-        raiseError('Bounds west and east coordinate are the same value')
-    }
-    if (south === north) {
-        raiseError('Bounds south and north coordinate are the same value')
-    }
+  const [west, south, east, north] = bounds;
+  if (west === east) {
+    raiseError("Bounds west and east coordinate are the same value");
+  }
+  if (south === north) {
+    raiseError("Bounds south and north coordinate are the same value");
+  }
 }
 
-let style = null
-let styleDir = null
+let style = null;
+let styleDir = null;
 
-if (styleProvided === 'yes') {
-    const stylePath = path.resolve(process.cwd(), styleLocation)
-    styleDir = path.dirname(stylePath)
-    style = JSON.parse(fs.readFileSync(stylePath, 'utf-8'))
+if (styleProvided === "yes") {
+  const stylePath = path.resolve(process.cwd(), styleLocation);
+  styleDir = path.dirname(stylePath);
+  style = JSON.parse(fs.readFileSync(stylePath, "utf-8"));
 }
 
-console.log('\n\n-------- Creating Maplibre GL map tiles --------')
+console.log("\n\n-------- Creating Maplibre GL map tiles --------");
 
-console.log('style provided: %j', styleProvided)
-if (styleLocation) console.log('style location: %j', styleLocation)
-if (sourceDir) console.log('local source path: %j', sourceDir)
-if (onlineSource) console.log('online source: %j', onlineSource)
-if (onlineSourceAPIKey) console.log('api key: %j', onlineSourceAPIKey)
-if (overlaySource) console.log('overlay source: %j', overlaySource)
-console.log('bounds: %j', bounds)
-console.log('minZoom: %j', minZoom)
-console.log('maxZoom: %j', maxZoom)
-console.log('output: %j', output)
-console.log('------------------------------------------------')
+console.log("style provided: %j", styleProvided);
+if (styleLocation) console.log("style location: %j", styleLocation);
+if (sourceDir) console.log("local source path: %j", sourceDir);
+if (onlineSource) console.log("online source: %j", onlineSource);
+if (onlineSourceAPIKey) console.log("api key: %j", onlineSourceAPIKey);
+if (overlaySource) console.log("overlay source: %j", overlaySource);
+console.log("bounds: %j", bounds);
+console.log("minZoom: %j", minZoom);
+console.log("maxZoom: %j", maxZoom);
+console.log("output: %j", output);
+console.log("------------------------------------------------");
 
-initiateRendering(styleProvided === 'yes', style, styleDir, sourceDir, onlineSource, onlineSourceAPIKey, overlaySource, bounds, minZoom, maxZoom, output)
+initiateRendering(
+  styleProvided === "yes",
+  style,
+  styleDir,
+  sourceDir,
+  onlineSource,
+  onlineSourceAPIKey,
+  overlaySource,
+  bounds,
+  minZoom,
+  maxZoom,
+  output,
+);
--- src/generate_resources.js
+++ src/generate_resources.js
@@ -1,16 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-import axios from 'axios';
+import fs from "fs";
+import path from "path";
+import axios from "axios";
 import sharp from "sharp";
-import { convertCoordinatesToTiles } from './tile_calculations.js';
+import { convertCoordinatesToTiles } from "./tile_calculations.js";
 
 // Download XYZ tile
 const downloadXyzTile = async (xyzUrl, filename, onlineSourceAPIKey) => {
   if (fs.existsSync(filename)) return false;
 
-  const config = { responseType: 'arraybuffer' };
+  const config = { responseType: "arraybuffer" };
   if (onlineSourceAPIKey) {
-    config.headers = { 'Authorization': `Bearer ${onlineSourceAPIKey}` };
+    config.headers = { Authorization: `Bearer ${onlineSourceAPIKey}` };
   }
 
   try {
@@ -19,7 +19,9 @@ const downloadXyzTile = async (xyzUrl, filename, onlineSourceAPIKey) => {
       fs.writeFileSync(filename, response.data);
       return true; // Return true if download was successful
     } else {
-      console.log(`Failed to download: ${xyzUrl} (Status code: ${response.status})`);
+      console.log(
+        `Failed to download: ${xyzUrl} (Status code: ${response.status})`,
+      );
       return false;
     }
   } catch (error) {
@@ -29,35 +31,51 @@ const downloadXyzTile = async (xyzUrl, filename, onlineSourceAPIKey) => {
 };
 
 // Download satellite imagery tiles from Bing Maps
-const downloadNaturalEarthTile = async (bounds, onlineSourceAPIKey, maxZoom, tempDir) => {
+const downloadNaturalEarthTile = async (
+  bounds,
+  onlineSourceAPIKey,
+  maxZoom,
+  tempDir,
+) => {
   if (!bounds || bounds.length < 4) {
-    console.error('Invalid bounds provided');
+    console.error("Invalid bounds provided");
     return;
   }
 
-  const rasterImageryUrl = "http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1";
+  const rasterImageryUrl =
+    "http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1";
   const rasterImageryAttribution = "© Microsoft (Bing Maps)";
-  
-  const xyzOutputDir = tempDir + 'tiles/';
+
+  const xyzOutputDir = tempDir + "tiles/";
   if (!fs.existsSync(xyzOutputDir)) {
     fs.mkdirSync(xyzOutputDir, { recursive: true });
   }
 
-  console.log('Downloading satellite imagery raster XYZ tiles from Bing VirtualEarth...');
+  console.log(
+    "Downloading satellite imagery raster XYZ tiles from Bing VirtualEarth...",
+  );
 
   // Iterate over zoom levels and tiles
   // Much of the below code is adapted from Microsoft's Bing Maps Tile System documentation:
   // https://learn.microsoft.com/en-us/bingmaps/articles/bing-maps-tile-system
   for (let zoom = 1; zoom <= maxZoom; zoom++) {
-    let { x: minX, y: maxY } = convertCoordinatesToTiles(bounds[0], bounds[1], zoom);
-    let { x: maxX, y: minY } = convertCoordinatesToTiles(bounds[2], bounds[3], zoom);
+    let { x: minX, y: maxY } = convertCoordinatesToTiles(
+      bounds[0],
+      bounds[1],
+      zoom,
+    );
+    let { x: maxX, y: minY } = convertCoordinatesToTiles(
+      bounds[2],
+      bounds[3],
+      zoom,
+    );
 
     let tileCount = 0;
 
     for (let col = minX; col <= maxX; col++) {
       for (let row = minY; row <= maxY; row++) {
         // Calculate quadkey
-        let quadkey = '';
+        let quadkey = "";
         for (let i = zoom; i > 0; i--) {
           let digit = 0;
           const mask = 1 << (i - 1);
@@ -66,17 +84,26 @@ const downloadNaturalEarthTile = async (bounds, onlineSourceAPIKey, maxZoom, tem
           quadkey += digit.toString();
         }
 
-        const xyzUrl = rasterImageryUrl.replace('{q}', quadkey);
-        const filename = path.join(xyzOutputDir, `${zoom}`, `${col}`, `${row}.jpg`);
+        const xyzUrl = rasterImageryUrl.replace("{q}", quadkey);
+        const filename = path.join(
+          xyzOutputDir,
+          `${zoom}`,
+          `${col}`,
+          `${row}.jpg`,
+        );
         if (!fs.existsSync(path.dirname(filename))) {
           fs.mkdirSync(path.dirname(filename), { recursive: true });
         }
         // Ideally, this would be done using Promise.all() to download multiple tiles at once,
         // But then we run into rate limiting issues with the Bing Maps API
         // https://learn.microsoft.com/en-us/bingmaps/getting-started/bing-maps-api-best-practices
-        const downloadSuccess = await downloadXyzTile(xyzUrl, filename, onlineSourceAPIKey);
+        const downloadSuccess = await downloadXyzTile(
+          xyzUrl,
+          filename,
+          onlineSourceAPIKey,
+        );
         if (downloadSuccess) tileCount++;
-        }
+      }
     }
     console.log(`Zoom level ${zoom} downloaded with ${tileCount} tiles`);
   }
@@ -84,24 +111,36 @@ const downloadNaturalEarthTile = async (bounds, onlineSourceAPIKey, maxZoom, tem
   // Save metadata.json file with proper attribution according to the Bing terms of use
   const metadata = {
     name: "Bing",
-    description: 'Satellite imagery from Bing maps',
-    version: '1.0.0',
+    description: "Satellite imagery from Bing maps",
+    version: "1.0.0",
     attribution: rasterImageryAttribution,
-    format: 'jpg',
-    type: 'overlay'
+    format: "jpg",
+    type: "overlay",
   };
 
-  const metadataFilePath = path.join(xyzOutputDir, 'metadata.json');
+  const metadataFilePath = path.join(xyzOutputDir, "metadata.json");
   fs.writeFileSync(metadataFilePath, JSON.stringify(metadata, null, 4));
 };
 
 // Handler for downloading remote tiles from different sources
-export const downloadRemoteTiles = (onlineSource, onlineSourceAPIKey, bounds, minZoom, maxZoom, tempDir) => {
+export const downloadRemoteTiles = (
+  onlineSource,
+  onlineSourceAPIKey,
+  bounds,
+  minZoom,
+  maxZoom,
+  tempDir,
+) => {
   return new Promise(async (resolve, reject) => {
     try {
       switch (onlineSource) {
         case "bing":
-          await downloadNaturalEarthTile(bounds, onlineSourceAPIKey, maxZoom, tempDir);
+          await downloadNaturalEarthTile(
+            bounds,
+            onlineSourceAPIKey,
+            maxZoom,
+            tempDir,
+          );
           resolve();
           break;
         default:
@@ -116,29 +155,29 @@ export const downloadRemoteTiles = (onlineSource, onlineSourceAPIKey, bounds, mi
 // Generate a Mapbox GL style JSON object from a remote source and an additional source.
 export const generateStyle = (onlineSource, overlaySource) => {
   const style = {
-    "version": 8,
-    "sources": {
+    version: 8,
+    sources: {
       [`${onlineSource}`]: {
-        "type": "raster",
-        "scheme": "xyz",
-        "tilejson": "2.2.0",
-        "tiles": [`tiles/{z}/{x}/{y}.jpg`],
-        "tileSize": 256,
-      }
+        type: "raster",
+        scheme: "xyz",
+        tilejson: "2.2.0",
+        tiles: [`tiles/{z}/{x}/{y}.jpg`],
+        tileSize: 256,
+      },
     },
-    "layers": [
+    layers: [
       {
-        "id": "background",
-        "type": "background",
-        "paint": {
+        id: "background",
+        type: "background",
+        paint: {
           "background-color": "#f9f9f9",
         },
       },
       {
-        "id": `${onlineSource}`,
-        "type": "raster",
-        "source": `${onlineSource}`,
-        "paint": {},
+        id: `${onlineSource}`,
+        type: "raster",
+        source: `${onlineSource}`,
+        paint: {},
       },
     ],
   };
@@ -146,27 +185,27 @@ export const generateStyle = (onlineSource, overlaySource) => {
   // transparent red fill and red outline.
   if (overlaySource) {
     style.sources["overlay"] = {
-      "type": "geojson",
-      "data": `overlay.geojson`,
+      type: "geojson",
+      data: `overlay.geojson`,
     };
     style.layers.push({
-      "id": "polygon-layer",
-      "type": "fill",
-      "source": "overlay",
+      id: "polygon-layer",
+      type: "fill",
+      source: "overlay",
       "source-layer": "output",
-      "filter": ["==", "$type", "Polygon"],
-      "paint": {
+      filter: ["==", "$type", "Polygon"],
+      paint: {
         "fill-color": "#FF0000",
         "fill-opacity": 0.5,
       },
     });
     style.layers.push({
-      "id": "line-layer",
-      "type": "line",
-      "source": "overlay",
+      id: "line-layer",
+      type: "line",
+      source: "overlay",
       "source-layer": "output",
-      "filter": ["==", "$type", "LineString"],
-      "paint": {
+      filter: ["==", "$type", "LineString"],
+      paint: {
         "line-color": "#FF0000",
         "line-width": 2,
       },
@@ -207,4 +246,3 @@ export const generateJPG = async (buffer, width, height, ratio) => {
     .jpeg()
     .toBuffer();
 };
-
