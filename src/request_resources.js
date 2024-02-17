bf8956ee1dfde352575af586b0f582584a23fdd9
Andrew Calcutt <acalcutt@techidiots.net>
2024-02-17 01:24:58 -0500

    try to add pmtiles support

--- src/request_resources.js
+++ src/request_resources.js
@@ -2,19 +2,47 @@ import fs from "fs";
 import path from "path";
 import zlib from "zlib";
 import MBTiles from "@mapbox/mbtiles";
+import { PMTiles, FetchSource } from "pmtiles";
 import https from "https";
 
 // Validations for source URLs
-const TILE_REGEXP = RegExp("mbtiles://([^/]+)/(\\d+)/(\\d+)/(\\d+)");
+const MBTILES_REGEXP = RegExp("mbtiles://([^/]+)/(\\d+)/(\\d+)/(\\d+)");
+const PMTILES_REGEXP = RegExp("pmtiles://([^/]+)(?:/)?.*/(\\d+)/(\\d+)/(\\d+)");
+const HTTP_REGEX = RegExp("^http(s)?://");
 const XYZ_REGEXP = /(\d+)\/(\d+)\/(\d+)\.(jpg|png|pbf|mvt)$/;
 const isMBTilesURL = (url) => url.startsWith("mbtiles://");
+const isPMTilesURL = (url) => url.startsWith("pmtiles://");
 const isGeoJSONURL = (url) => url.endsWith(".geojson");
 const isProtomapsTileJSONURL = (url) => url.endsWith("protomaps-tiles.json");
 const isXYZDirURL = (url) => /\/?\d+\/\d+\/\d+(\.\w+)?$/.test(url);
-const isOnlineURL = (url) => url.startsWith("http");
+const isOnlineURL = (url) => url.match(HTTP_REGEX);
 
 // Split out mbtiles service name from the URL
 const resolveNamefromURL = (url) => url.split("://")[1].split("/")[0];
+const resolveNamefromPMtilesURL = (url) => url.split("pmtiles://")[1];
+
+// Resolve a URL of a local mbtiles file to a file path
+// Expected to follow this format "mbtiles://<service_name>/*"
+const resolvePMTilesURL = (sourceDir, url) => {
+  /*
+   * @param {String} sourceDir - path containing mbtiles files
+   * @param {String} url - url of a data source in style.json file.
+   */
+
+  const pmtilesFile = resolveNamefromPMtilesURL(url);
+  const isHttp = pmtilesFile.match(HTTP_REGEX);
+  if (isHttp) {
+    return pmtilesFile;
+  } else {
+    return path.format({
+      dir: sourceDir,
+      name: resolveNamefromURL(url),
+      ext: ".mbtiles",
+    });
+  }
+
+  return resolveNamefromPMtilesURL(url);
+};
 
 // Resolve a URL of a local mbtiles file to a file path
 // Expected to follow this format "mbtiles://<service_name>/*"
@@ -62,8 +90,8 @@ const getLocalSpriteJSON = (styleDir, url, callback) => {
   });
 };
 
-// Given a URL to an online glyph, get the glyph data.
-const getOnlineGlyph = (url, callback) => {
+// Given a URL to an online data, get the data.
+const getOnlineData = (url, callback) => {
   https
     .get(url, (res) => {
       let data = [];
@@ -94,6 +122,77 @@ const getLocalGlyph = (styleDir, url, callback) => {
     callback(null, { data });
     return null;
   });
+};
+
+// Given a URL to a pmtiles file, get the TileJSON for that to load correct tiles.
+const getPMTilesTileJSON = async (sourceDir, url, callback) => {
+  /*
+   * @param {String} sourceDir - path containing mbtiles files.
+   * @param {String} url - url of a data source in style.json file.
+   * @param {function} callback - function to call with (err, {data}).
+   */
+  const pmtilesFile = resolvePMTilesURL(sourceDir, url);
+  const service = resolveNamefromPMtilesURL(url);
+  const isHttp = pmtilesFile.match(HTTP_REGEX);
+
+  let pmtiles = undefined;
+  if (isHttp) {
+    const source = new FetchSource(pmtilesFile);
+    pmtiles = new PMTiles(source);
+  } else {
+    const fd = fs.openSync(pmtilesFile, "r");
+    const source = new PMTilesFileSource(fd);
+    pmtiles = new PMTiles(source);
+  }
+
+  const header = await pmtiles.getHeader();
+  const metadata = await pmtiles.getMetadata();
+
+  //Add missing metadata from header
+  metadata["format"] = getPmtilesTileType(header.tileType).type;
+  metadata["minzoom"] = header.minZoom;
+  metadata["maxzoom"] = header.maxZoom;
+
+  if (header.minLon && header.minLat && header.maxLon && header.maxLat) {
+    metadata["bounds"] = [
+      header.minLon,
+      header.minLat,
+      header.maxLon,
+      header.maxLat,
+    ];
+  } else {
+    metadata["bounds"] = [-180, -85.05112877980659, 180, 85.0511287798066];
+  }
+
+  if (header.centerZoom) {
+    metadata["center"] = [
+      header.centerLon,
+      header.centerLat,
+      header.centerZoom,
+    ];
+  } else {
+    metadata["center"] = [
+      header.centerLon,
+      header.centerLat,
+      parseInt(metadata["maxzoom"]) / 2,
+    ];
+  }
+
+  const { minzoom, maxzoom, center, bounds, format } = metadata;
+
+  const ext = format === "pbf" ? ".pbf" : "";
+
+  const tileJSON = {
+    tilejson: "1.0.0",
+    tiles: [`pmtiles://${service}/{z}/{x}/{y}${ext}`],
+    minzoom,
+    maxzoom,
+    center,
+    bounds,
+  };
+
+  callback(null, { data: Buffer.from(JSON.stringify(tileJSON)) });
+  return null;
 };
 
 // Given a URL to a local mbtiles file, get the TileJSON for that to load correct tiles.
@@ -140,13 +239,47 @@ const getLocalMBTileJSON = (sourceDir, url, callback) => {
 };
 
 // Fetch a tile from a local mbtiles file.
+const getPMTiles = async (sourceDir, url, callback) => {
+  /*
+   * @param {String} sourceDir - path containing pmtiles files.
+   * @param {String} url - url of a data source in style.json file.
+   * @param {function} callback - function to call with (err, {data}).
+   */
+  const matches = url.match(PMTILES_REGEXP);
+  const [z, x, y] = matches.slice(matches.length - 3, matches.length);
+  const parts = url.split("/");
+  parts.splice(-3);
+  const pmtilesFile = resolvePMTilesURL(sourceDir, parts.join("/"));
+  const isHttp = pmtilesFile.match(HTTP_REGEX);
+
+  let pmtiles = undefined;
+  if (isHttp) {
+    const source = new FetchSource(pmtilesFile);
+    pmtiles = new PMTiles(source);
+  } else {
+    const fd = fs.openSync(pmtilesFile, "r");
+    const source = new PMTilesFileSource(fd);
+    pmtiles = new PMTiles(source);
+  }
+
+  let zxyTile = await pmtiles.getZxy(z, x, y);
+  if (zxyTile && zxyTile.data) {
+    const data = Buffer.from(zxyTile.data);
+    callback(null, { data });
+  } else {
+    callback(null, {});
+    return null;
+  }
+};
+
+// Fetch a tile from a local mbtiles file.
 const getLocalMBTile = (sourceDir, url, callback) => {
   /*
    * @param {String} sourceDir - path containing mbtiles files.
    * @param {String} url - url of a data source in style.json file.
    * @param {function} callback - function to call with (err, {data}).
    */
-  const matches = url.match(TILE_REGEXP);
+  const matches = url.match(MBTILES_REGEXP);
   const [z, x, y] = matches.slice(matches.length - 3, matches.length);
   const isVector = path.extname(url) === ".pbf";
   const mbtilesFile = resolveMBTilesURL(sourceDir, url);
@@ -261,6 +394,8 @@ export const requestHandler =
           // source
           if (isMBTilesURL(url)) {
             getLocalMBTileJSON(sourceDir, url, callback);
+          } else if (isPMTilesURL(url)) {
+            getPMTilesTileJSON(sourceDir, url, callback);
           } else if (isXYZDirURL(url)) {
             getLocalXYZTile(sourceDir, url, callback);
           } else if (isGeoJSONURL(url)) {
@@ -277,6 +412,8 @@ export const requestHandler =
           // tile
           if (isMBTilesURL(url)) {
             getLocalMBTile(sourceDir, url, callback);
+          } else if (isPMTilesURL(url)) {
+            getPMTiles(sourceDir, url, callback);
           } else if (isXYZDirURL(url)) {
             getLocalXYZTile(sourceDir, url, callback);
           } else if (isGeoJSONURL(url)) {
@@ -290,7 +427,7 @@ export const requestHandler =
         case 4: {
           // glyph
           if (isOnlineURL(url)) {
-            getOnlineGlyph(url, callback);
+            getOnlineData(url, callback);
           } else {
             getLocalGlyph(styleDir, url, callback);
           }
@@ -298,12 +435,20 @@ export const requestHandler =
         }
         case 5: {
           // sprite image
-          getLocalSpriteImage(styleDir, url, callback);
+          if (isOnlineURL(url)) {
+            getOnlineData(url, callback);
+          } else {
+            getLocalSpriteImage(styleDir, url, callback);
+          }
           break;
         }
         case 6: {
           // sprite json
-          getLocalSpriteJSON(styleDir, url, callback);
+          if (isOnlineURL(url)) {
+            getOnlineData(url, callback);
+          } else {
+            getLocalSpriteJSON(styleDir, url, callback);
+          }
           break;
         }
         default: {
@@ -316,3 +461,69 @@ export const requestHandler =
       callback(msg);
     }
   };
+
+class PMTilesFileSource {
+  constructor(fd) {
+    this.fd = fd;
+  }
+  getKey() {
+    return this.fd;
+  }
+  async getBytes(offset, length) {
+    const buffer = Buffer.alloc(length);
+    await readFileBytes(this.fd, buffer, offset);
+    const ab = buffer.buffer.slice(
+      buffer.byteOffset,
+      buffer.byteOffset + buffer.byteLength,
+    );
+    return { data: ab };
+  }
+}
+
+/**
+ *
+ * @param fd
+ * @param buffer
+ * @param offset
+ */
+async function readFileBytes(fd, buffer, offset) {
+  return new Promise((resolve, reject) => {
+    fs.read(fd, buffer, 0, buffer.length, offset, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+function getPmtilesTileType(typenum) {
+  let head = {};
+  let tileType;
+  switch (typenum) {
+    case 0:
+      tileType = "Unknown";
+      break;
+    case 1:
+      tileType = "pbf";
+      head["Content-Type"] = "application/x-protobuf";
+      break;
+    case 2:
+      tileType = "png";
+      head["Content-Type"] = "image/png";
+      break;
+    case 3:
+      tileType = "jpeg";
+      head["Content-Type"] = "image/jpeg";
+      break;
+    case 4:
+      tileType = "webp";
+      head["Content-Type"] = "image/webp";
+      break;
+    case 5:
+      tileType = "avif";
+      head["Content-Type"] = "image/avif";
+      break;
+  }
+  return { type: tileType, header: head };
+}
