244ecfc964762b279985d4ff876e793830a1cafd
Andrew Calcutt <acalcutt@techidiots.net>
2024-02-19 22:23:46 -0500

    Add options for ratio and tileType (#44)
    * Add options for ratio and tileType
    
    * remove console.log from testing
    
    * use new sharp premultiplied option
    
    * change variable order to look nicer
    
    * Update README.md
    
    * Update README.md

--- src/cli.js
+++ src/cli.js
@@ -52,6 +52,7 @@ program
     "(Required) Maximum zoom level",
     parseInt,
   )
+  .option("-r, --ratio <number>", "Map pixel ratio (default 1)", Math.floor, 1)
   .option(
     "-o, --outputdir <type>",
     "Output directory (default 'outputs/')",
@@ -61,6 +62,17 @@ program
     "-f, --filename <type>",
     "Output filename (default 'output')",
     "output",
+  )
+  .option(
+    "-t, --tiletype <type>",
+    "Tile type (jpg, png, or webp)",
+    (type) => {
+      if (!["jpg", "png", "webp"].includes(type)) {
+        throw new Error("Invalid tile type");
+      }
+      return type;
+    },
+    "jpg",
   );
 
 program.parse(process.argv);
@@ -76,10 +88,12 @@ const {
   openstreetmap: openStreetMap,
   overlay,
   bounds,
+  ratio,
   minzoom: minZoom,
   maxzoom: maxZoom,
   outputdir: outputDir,
   filename: outputFilename,
+  tiletype,
 } = options;
 
 validateInputOptions(
@@ -92,8 +106,10 @@ validateInputOptions(
   openStreetMap,
   overlay,
   bounds,
+  ratio,
   minZoom,
   maxZoom,
+  tiletype,
 );
 
 console.log("\n\n-------- Rendering map tiles with Maplibre GL --------");
@@ -111,8 +127,10 @@ if (overlay) console.log("Overlay: %j", overlay);
 console.log("Bounding box: %j", bounds);
 console.log("Min zoom: %j", minZoom);
 console.log("Max zoom: %j", maxZoom);
+console.log("Ratio: %j", ratio);
 console.log("Output directory: %j", outputDir);
 console.log("Output MBTiles filename: %j", outputFilename);
+console.log("Output Tile Type: %j", tiletype);
 console.log("------------------------------------------------------");
 
 const renderResult = await initiateRendering(
@@ -125,10 +143,12 @@ const renderResult = await initiateRendering(
   openStreetMap,
   overlay,
   bounds,
+  ratio,
   minZoom,
   maxZoom,
   outputDir,
   outputFilename,
+  tiletype,
 );
 
 // output the render result to console
