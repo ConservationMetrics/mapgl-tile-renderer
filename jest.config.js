4409b3bf3e975bbd3e2e868b094718569ba01b6e
Rudo Kemper <rtakemper@gmail.com>
2024-11-26 15:15:09 -0500

    Alias styled-map-package

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ export default {
   },
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
+    "^styled-map-package/from-mbtiles$":
+      "<rootDir>/node_modules/styled-map-package/lib/from-mbtiles.js",
+    "^styled-map-package/writer$":
+      "<rootDir>/node_modules/styled-map-package/lib/writer.js",
   },
   transformIgnorePatterns: [], // Remove the ignore pattern to transform all modules
 };
