56b5024341e453af38a719351bf1de14bd2f5a7b
Rudo Kemper <rtakemper@gmail.com>
2025-10-08 16:28:26 -0400

    Correct mapping for smp module

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,9 @@ export default {
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
     "^styled-map-package/from-mbtiles$":
-      "<rootDir>/node_modules/styled-map-package/lib/from-mbtiles.js",
+      "<rootDir>/node_modules/styled-map-package/dist/from-mbtiles.js",
     "^styled-map-package/writer$":
-      "<rootDir>/node_modules/styled-map-package/lib/writer.js",
+      "<rootDir>/node_modules/styled-map-package/dist/writer.js",
   },
   transformIgnorePatterns: [], // Remove the ignore pattern to transform all modules
 };
